6fb9f76657d31a050e37e5f7001d8839c98a9514
KravAD <dkrav@myuax.com>
2023-11-30 12:13:06 +0100

    commit 6
    Declaro file(file name) y condición de si el archivo no está abierto, saca que da error.

--- tiny-lisp.cpp
+++ tiny-lisp.cpp
@@ -5,4 +5,10 @@ using namespace std;
 
 void load_script (const char* filename, bool show_script = false) {
 
+    ifstream file(filename);
+    if (!file.is.open()) {
+        cerr << "Error: No se pudo abrir el archivo " << filename << endl;
+        return;
+    }
+
 }
